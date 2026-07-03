e53243b7f825eaab7559412edde1a7e929c4963a
B <hang020713@gmail.com>
2026-07-03 12:08:18 +0800

    chore: added the gpio, rpicam to install_server script

--- install_server.bash
+++ install_server.bash
@@ -236,6 +236,64 @@ sudo apt install -y \
 echo "DONE installing cartographer_ros dependencies"
 sleep 1
 
+# pinctrl
+sudo apt update
+sudo apt install -y cmake git device-tree-compiler build-essential libncurses5-dev libncursesw5-dev libfdt-dev
+
+cd ~
+git clone https://github.com/raspberrypi/utils.git
+cd utils
+
+cmake .
+make
+sudo make install
+
+cd pinctrl
+cmake .
+make
+sudo make install
+
+# rpicam
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y git clang meson ninja-build pkg-config libyaml-dev python3-yaml python3-ply python3-jinja2 openssl
+sudo apt install -y libdw-dev libunwind-dev libudev-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libpython3-dev pybind11-dev libevent-dev libtiff-dev qt6-base-dev qt6-tools-dev-tools liblttng-ust-dev lttng-tools libexif-dev libjpeg-dev libgtest-dev abi-compliance-checker
+sudo apt install -y cmake libboost-program-options-dev libdrm-dev libexif-dev ffmpeg libavcodec-extra libavcodec-dev libavdevice-dev libpng-dev libpng-tools libepoxy-dev qt5-qmake qtmultimedia5-dev
+
+# Clone the Raspberry Pi's libcamera repository
+cd ~
+git clone https://github.com/raspberrypi/libcamera.git
+cd libcamera    
+
+# Configure the build
+meson setup build --buildtype=release -Dpipelines=rpi/vc4,rpi/pisp -Dipas=rpi/vc4,rpi/pisp -Dv4l2=true -Dgstreamer=enabled -Dtest=false -Dlc-compliance=disabled -Dcam=disabled -Dqcam=disabled -Ddocumentation=disabled -Dpycamera=enabled
+
+# Compile and install
+sudo ninja -C build install
+
+
+# Go back to your home directory or another suitable location
+cd ~
+
+# Clone the rpicam-apps repository
+git clone https://github.com/raspberrypi/rpicam-apps.git
+cd rpicam-apps/
+
+# Configure the build. (Enable the features you need)
+meson setup build -Denable_libav=disabled -Denable_drm=enabled -Denable_egl=enabled -Denable_qt=enabled -Denable_opencv=disabled -Denable_tflite=disabled -Denable_hailo=disabled
+
+# Compile and install
+meson compile -C build
+sudo meson install -C build
+sudo ldconfig
+
+# Add user to group permission
+sudo usermod -aG video $USER
+
+# gpio
+sudo apt update
+sudo apt install -y gpiod libgpiod-dev python3-libgpiod python3-pip python3-gpiozero python3-lgpio
+sudo usermod -aG dialout $USER
+
 # Re-enable background apt services
 echo "Re-enabling automatic apt services..."
 sudo systemctl start apt-daily.timer apt-daily-upgrade.timer 2>/dev/null || true
