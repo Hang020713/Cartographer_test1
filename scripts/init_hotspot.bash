df3ba3af407b9339ff23833af2b1fe2455a1b441
B <hang020713@gmail.com>
2026-07-03 09:20:05 +0800

    fix: update the nmcli. and enable ubuntu server ssh server

--- scripts/init_hotspot.bash
+++ scripts/init_hotspot.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# install network-manager
+sudo apt install -y network-manager
+
 # Create hotspot connection
 sudo nmcli connection add \
   type wifi \
@@ -18,6 +21,16 @@ sudo nmcli connection modify Hotspot \
 # Enable the hotspot
 sudo nmcli connection up Hotspot
 
-# Enable SSH service
-sudo systemctl enable ssh
-sudo systemctl start ssh
+# Enable SSH service (raspbian)
+#sudo systemctl enable ssh
+#sudo systemctl start ssh
+
+# Enable SSH service (ubuntu)
+# sudo apt install openssh-server -y
+# sudo systemctl enable --now ssh
+# sudo systemctl start ssh
+# sudo ufw allow ssh
+# sudo nano /etc/ssh/sshd_config
+# PasswordAuthentication yes
+# KbdInteractiveAuthentication yes
+# sudo systemctl restart ssh
